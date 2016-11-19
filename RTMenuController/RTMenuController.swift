a669f063663e7bcf75ade9bec45106c518b3488c
RyuichiTanimoto <ryuichi.tanimoto@gmail.com>
2016-11-19 18:34:02 +0900

    create podspec

---
+++ RTMenuController/RTMenuController.swift
@@ -0,0 +1,9 @@
+//
+//  RTMenuController.swift
+//  RTMenuController
+//
+//  Created by Ryuichi Tanimoto on 11/19/16
+//  Copyright (c) 2016 Ryuichi Tanimoto. All rights reserved.
+//
+
+import UIKit
